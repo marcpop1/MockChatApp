b5f77e5601565d5a25ca7390a029668b5b2055ab
Marc <marcpop2003@yahoo.com>
2024-09-12 20:13:10 +0300

    Implemented sidebar hidden functionality when window size is reduced

--- mock-chat-app/src/App.tsx
+++ mock-chat-app/src/App.tsx
@@ -1,10 +1,16 @@
 import type { Component } from 'solid-js';
 import { Sidebar } from './sidebar/Sidebar';
+import styles from './App.module.css';
 
 const App: Component = () => {
+  const title: string = 'Mock Chat App';
+
   return (
     <div>
-      <Sidebar/>
+      <h1 class={styles.title}>{title}</h1>
+      <div class={styles.sidebar}>
+        <Sidebar/>
+      </div>
     </div>
   );
 };

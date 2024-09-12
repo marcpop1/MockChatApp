04107c3adc319fd5bb95293c26d0ac3d3af6f44b
Marc <marcpop2003@yahoo.com>
2024-09-12 21:11:23 +0300

    Added drag & drop functionality

--- mock-chat-app/src/sidebar/Sidebar.tsx
+++ mock-chat-app/src/sidebar/Sidebar.tsx
@@ -2,13 +2,17 @@ import { batch, Component, createSignal, For } from "solid-js";
 import { Contact } from "../contact/Contact";
 import { AddConversationButton } from "../add-conversation-button/AddConversationButton";
 import { DeleteConversationButton } from "../delete-conversation-button.tsx/DeleteConversationButton";
+import { dndzone } from "solid-dnd-directive";
+import styles from "./Sidebar.module.css"
+
+type ContactItem = { id: number, name: string };
 
 export const Sidebar: Component = () => {
     const title: string = 'Contacts';
-    const [contacts, setContacts] = createSignal<string[]>([
-        'Contact 1',
-        'Contact 2',
-        'Contact 3'
+    const [contacts, setContacts] = createSignal<ContactItem[]>([
+        { id: 1, name: 'Contact 1' },
+        { id: 2, name: 'Contact 2' },
+        { id: 3, name: 'Contact 3' }
     ]);
     const [newContactName, setNewContactName] = createSignal<string>('');
 
@@ -18,9 +22,9 @@ export const Sidebar: Component = () => {
         }
 
         batch(() => {
-            setContacts((contacts) => contacts = [...contacts, newContactName()]);
+            setContacts((contacts) => contacts = [...contacts, { id: contacts.length + 1, name: newContactName() }]);
         })
-        
+
         setNewContactName("");
     }
 
@@ -28,28 +32,40 @@ export const Sidebar: Component = () => {
         setContacts((contacts) => [...contacts.slice(0, index), ...contacts.slice(index + 1)]);
     }
 
+    const handleDndEvent = (e: any) => {
+        const { items: newContacts } = e.detail;
+        setContacts(newContacts);
+    }
+
     return (
         <div>
             <h2>{title}</h2>
-            <form>
-                <input
-                    placeholder="Enter contact name"
-                    required
-                    value={newContactName()}
-                    onInput={(e) => setNewContactName(e.currentTarget.value)}
-                />
             <div>
-                <AddConversationButton addConversation={addConversation} />
+                <form>
+                    <input
+                        placeholder="Enter contact name"
+                        required
+                        value={newContactName()}
+                        onInput={(e) => setNewContactName(e.currentTarget.value)}
+                    />
+                    <div>
+                        <AddConversationButton addConversation={addConversation} />
+                    </div>
+                </form>
             </div>
-            </form>
-            <ul>
+            <br />
+            <section
+                use:dndzone={{ items: contacts }}
+                on:consider={handleDndEvent}
+                on:finalize={handleDndEvent}
+            >
                 <For each={contacts()}>{(contact, i) =>
-                    <li>
-                        <Contact title={contact} />
+                    <div class={styles.item}>
+                        <Contact title={contact.name} />
                         <DeleteConversationButton deleteConversation={deleteConversation} index={i()} />
-                    </li>
+                    </div>
                 }</For>
-            </ul>
+            </section>
         </div>
     );
 };
